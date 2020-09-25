84a35d3529def7debba65a458ffec32549a09ae7
ClaytonWJ <cjennings@sensoft.ca>
2020-09-25 16:38:13 -0400

    Fixed logic error with r_idx in main algorithm for when the buffer doesn't expand right once. Fixed plotting function to accept the proccessed results dictionary and set that to the default result passed. Added docstrings to function and comments throughout code.

--- flatness.py
+++ flatness.py
@@ -6,19 +6,35 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 
 def process_results(results):
+    '''
+    Function to remove duplicate flat areas. It checks if any set of indexes are completely contained inside another set
+    and removes them if this is true. It them adds the valid results into a dictionary with the starting index as the key
+    :param results:
+    :return: result dictionary in form {"<starting index>": [(<min index>, <start index>, <max index>), # indexes]
+    '''
+    # convert the input results into a lists with the starting index and a range between the min and max index of the
+    # flat area
     indexes = [[idx, range(results[idx][0][0], results[idx][0][2]),
                            results[idx][1]] for idx in results.keys()]
     good_idxs = {}
+    # Check if any result's range is contained completely within any of the other results. If so, remove it as its
+    # a redundant result
     for i in indexes:
         if not any([ranges[1] for ranges in indexes if (not ranges == i and
                                                         i[1][0] in ranges[1] and
                                                         i[1][-1] in ranges[1])]):
             good_idxs[i[0]] = [(i[1][0], i[0], i[1][-1]),i[2]]
 
+    # Return the filtered results in a dictionary format
     return good_idxs
 
 
 def print_results(results):
+    '''
+    Function to print out results in a nice format
+    :param results:
+    :return:
+    '''
     for entry in results:
         if isinstance(entry, dict):
             print('+'*20)
@@ -31,25 +47,18 @@ def print_results(results):
             print("Indexes: " + str(entry[1]))
             print("Buffer Size: " + str(entry[2]))
 
-def plot_val(trace, k_r, results):
-
-    data_points_dic = {}
-    data_points = []
+def plot_val(trace, k_r, results, save_figure=False):
+    '''
+    Function to plot the results of the algorithm using matplotlib. The function will create a line plot of the original
+    data (x = trace number, y = Plot parameter) and plot the indexes identified as flat overtop the original data in red.
+    :param trace: List of Trace Numbers
+    :param k_r: List of Values
+    :param results: Result dictionary from flatness algorithm
+    :return:
+    '''
+    # data_points_dic = {}
+    # data_points = []
     segments = [(results[x][0][0], results[x][0][2]) for x in results]
-
-    for entry in results:
-        # this works when you don't run this in the function process_results()
-        l_idx, _ , r_idx = results[entry][0]
-        stdev = results[entry][2]
-
-        for idx in range(l_idx, r_idx+1):
-            data_points_dic[idx] = stdev
-
-    for idx in range(len(trace)):
-        if idx in data_points_dic:
-            data_points.append(0)
-        else:
-            data_points.append(k_r[idx])
 
     x_points = trace
     y_points = k_r
@@ -64,39 +73,56 @@ def plot_val(trace, k_r, results):
     plt.plot(x_points, y_points)
     for seg in segments:
         plt.plot(x_points[seg[0]:seg[1]], y_points[seg[0]:seg[1]], color='r')
-    # plt.colorbar()
-    # plt.show()
-    plt.savefig("fig_range.png")
+
+    if save_figure:
+        plt.savefig("flatness_example.png")
+    else:
+        plt.show()
 
 def flatness (inputTrace, indexs):
+    '''
+    Algorithm designed to identify flat areas within a pdp line dataset. The algorithm uses a set buffer size and a
+    threshold for standard deviation to identify flat areas. If the points within the buffer are under the threshold it
+    then trys to expand the buffer to the left and right as much as possible by check if the next point has a value
+    equal to or less than 2 standard deviations away from the mean of the original buffer points. Once it fails this
+    condition in both directions it returns the result.
+    :param inputTrace:
+    :param indexs:
+    :return:
+    '''
     result = {}
+    # Buffer size controls and number of points that are checked at once. It is the minimum # of points that used to
+    # check if an area is considered flat
     buffer_size = 20
-    # threshold determines the stdev value that cannot be exceded. Should consider wether to use
-    # static threshold or variable (percent based increase. ie stdv < 0.01 then cannot excede 20% increase
+    # threshold determines the standard deviation value that is checked to determine if an area is flat
     threshold = 0.025
     idx = buffer_size
 
     while (idx in indexs[buffer_size:]):
+        # Set the buffer to the x number of points before the current index
         buffer = inputTrace[(idx-buffer_size):idx]
+        # calculate the standard deviation on the point within the buffer and check if it meats the threshold condition
         stdev = statistics.stdev(buffer)
         if stdev < threshold:
             # left and right used to track if buffer should expand in either direction
             left = True
             right = True
             # index of expanded buffer on right and left
-            r_idx = idx
+            r_idx = idx + 1
             l_idx = idx-buffer_size
+
             mean = sum(buffer)/len(buffer)
+            # Calculate 2 standard deviations above and below and use it as the range of accepted values
             allowed_range = (mean - 2*stdev, mean + 2*stdev)
             while left or right:
-            # Should consider if moving right first then left each loop is ideal. What about right as far as possible
-            # then left, or visa vera. Or expanding buffer at the same time.
+                # Expansion loop. This loop will attempt to add points to the existing buffer based on if the new point
+                # is within the allowed_range. When it cannot expand further it returns the resulting list of points
                 if right:
                     if r_idx >= len(inputTrace):
                         # stop expanding right if passed last index
                         right = False
                         continue
-
+                    # if the value is within the allowed_range add it to the buffer and move the right index
                     if allowed_range[0] <= inputTrace[r_idx] <= allowed_range[1]:
                         buffer.append(inputTrace[r_idx])
                         r_idx += 1
@@ -107,15 +133,16 @@ def flatness (inputTrace, indexs):
                         # stop expanding left if past first index
                         left = False
                         continue
-
+                    # if the value is within the allowed_range add it to the buffer and move the left index
                     if allowed_range[0] <= inputTrace[l_idx] <= allowed_range[1]:
                         buffer.append(inputTrace[l_idx])
                         l_idx -= 1
                     else:
                         left = False
+            # remove the +/- from left and right index because it always ends on a failed check
             result[idx] = ([(l_idx+1, idx, r_idx-1), len(buffer), statistics.stdev(buffer)])
-            # set idx to the last right index so the loop starts after the end of the last flat area
-            idx = r_idx
+            # set idx to the last right index + buffer size so the main loop checks the next new points in the dataset
+            idx = r_idx + buffer_size - 1
         # Increment main loop index
         idx += 1
         
@@ -151,10 +178,7 @@ if __name__ == "__main__":
 
     results = eval(algorithm)(trace_k, indexs)
 
-    temp = process_results(results)
+    processed_results = process_results(results)
 
-    # change standard deviation calculation to use a fixed average from the original buffer.
-    # should make the algorithm more likely to trip threshold on gradual change.
-
-    plot_val(trace, trace_k, results)
+    plot_val(trace, trace_k, processed_results, save_figure=True)
     
