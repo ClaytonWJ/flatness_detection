c0fad36e85f3f70727ed2e5a23500de3910d8317
ClaytonWJ <cjennings@sensoft.ca>
2020-09-28 09:40:04 -0400

    Setup to run in batch operation on all data in example folder, verbose or regular. Added file naming for plot output. Updated threshold for algorithm to be 0.05

--- flatness.py
+++ flatness.py
@@ -1,11 +1,12 @@
 import numpy as np
+import datetime
 import statistics
 import os
 import sys
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 
-def process_results(results):
+def remove_duplicate_ranges(results):
     '''
     Function to remove duplicate flat areas. It checks if any set of indexes are completely contained inside another set
     and removes them if this is true. It them adds the valid results into a dictionary with the starting index as the key
@@ -23,7 +24,7 @@ def process_results(results):
         if not any([ranges[1] for ranges in indexes if (not ranges == i and
                                                         i[1][0] in ranges[1] and
                                                         i[1][-1] in ranges[1])]):
-            good_idxs[i[0]] = [(i[1][0], i[0], i[1][-1]),i[2]]
+            good_idxs[i[0]] = [(i[1][0], i[0], i[1][-1]), i[2]]
 
     # Return the filtered results in a dictionary format
     return good_idxs
@@ -47,7 +48,7 @@ def print_results(results):
             print("Indexes: " + str(entry[1]))
             print("Buffer Size: " + str(entry[2]))
 
-def plot_val(trace, k_r, results, save_figure=False):
+def plot_val(trace, k_r, results, save_figure=False, fig_name="flatness", fig_type="png"):
     '''
     Function to plot the results of the algorithm using matplotlib. The function will create a line plot of the original
     data (x = trace number, y = Plot parameter) and plot the indexes identified as flat overtop the original data in red.
@@ -68,16 +69,18 @@ def plot_val(trace, k_r, results, save_figure=False):
     plt.title("Flatness Detection")
     plt.xlabel("Trace")
     plt.ylabel("K_r")
-    plt.ylim((1,3))
+    plt.ylim((0,5))
 
     plt.plot(x_points, y_points)
     for seg in segments:
         plt.plot(x_points[seg[0]:seg[1]], y_points[seg[0]:seg[1]], color='r')
 
     if save_figure:
-        plt.savefig("flatness_example.png")
+        plt.savefig(fig_name + "." + fig_type)
     else:
         plt.show()
+
+    plt.close()
 
 def flatness (inputTrace, indexs):
     '''
@@ -95,14 +98,14 @@ def flatness (inputTrace, indexs):
     # check if an area is considered flat
     buffer_size = 20
     # threshold determines the standard deviation value that is checked to determine if an area is flat
-    threshold = 0.025
+    threshold = 0.05
     idx = buffer_size
 
     while (idx in indexs[buffer_size:]):
         # Set the buffer to the x number of points before the current index
         buffer = inputTrace[(idx-buffer_size):idx]
         # number of standard deviation to consider valid
-        stdv_factor = 2
+        stdv_factor = 3
         # calculate the standard deviation on the point within the buffer and check if it meats the threshold condition
         stdev = statistics.stdev(buffer)
         if stdev < threshold:
@@ -155,32 +158,43 @@ if __name__ == "__main__":
     
     args = sys.argv 
 
-    print (args)
     algorithm = "flatness"
 
-    # if len(args) == 2 and args[1] == "rl":
-    #     algorithm = "flatness_rl"
+    if len(args) == 2 and args[1] == "md":
+        algorithm = "flatness_mean_diff"
 
     print('='*45)
     print('=' + ' '*15 + 'Starting Run' + ' '*16 + '=')
     print('='*45)
+    start_time = datetime.datetime.now()
 
     # inputTrace = list(map(lambda x: round(x, 2), (np.random.rand(1,1000) *10).tolist()[0]))
     # indexs = list(range(0,len(inputTrace)))
-    datafile = "Line1.csv"
-    flpath = os.path.join(os.getcwd(), "example_data", datafile)
+    in_files = [x for x in os.listdir(os.path.join(os.getcwd(), "example_data")) if "Line" in x]
+    for fl in in_files:
+        datafile = fl
+        flpath = os.path.join(os.getcwd(), "example_data", datafile)
 
-    data = open(flpath, 'r').readlines()
-    formated_data = data
-    headers = data[48]
-    trace_k = [float(x.split(',')[3]) for x in data[50:]]
-    trace = [float(x.split(',')[0]) for x in data[50:]]
+        data = open(flpath, 'r').readlines()
+        formated_data = data
+        if 'verbose' in datafile:
+            headers = data[0]
+            trace_k = [float(x.split(',')[3]) for x in data[1:]]
+            trace = [float(x.split(',')[0]) for x in data[1:]]
+        else:
+            headers = data[48]
+            trace_k = [float(x.split(',')[3]) for x in data[50:]]
+            trace = [float(x.split(',')[0]) for x in data[50:]]
 
-    indexs = list(range(0,len(trace_k)))
+        indexs = list(range(0,len(trace_k)))
 
-    results = eval(algorithm)(trace_k, indexs)
+        results = eval(algorithm)(trace_k, indexs)
 
-    processed_results = process_results(results)
+        processed_results = remove_duplicate_ranges(results)
 
-    plot_val(trace, trace_k, processed_results, save_figure=True)
+        plot_val(trace, trace_k, processed_results, save_figure=True, fig_name=datafile.split('.')[0])
+
+    end_time = datetime.datetime.now()
+    time_delta = end_time - start_time
+    print ("S: {} - E: {}\nRun time: {:.2f}s".format(start_time, end_time, (time_delta.total_seconds())))
     
